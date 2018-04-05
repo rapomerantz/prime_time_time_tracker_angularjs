ba54b0d73afb4ec4113a83bc1800de03660a0577
Atticus Pomerantz <rapomerantz@gmail.com>
2018-04-04 19:30:50 -0500

    Done on Wednesday

--- server/public/scripts/controllers/tasks.controller.js
+++ server/public/scripts/controllers/tasks.controller.js
@@ -1,4 +1,11 @@
 app.controller('TasksController', [function () {
     console.log('TaskController has been loaded');
+    let self = this;
+
+    self.addProject = function (newProject) {
+        console.log('in addProject', newProject);
+        
+    }
+
 
 }])
